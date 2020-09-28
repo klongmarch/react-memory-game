51951a3bc8aefc984d0a1ef07da14ee0cb024a8f
steve kang <klongmarch@yahoo.com>
2020-09-28 11:34:21 -0500

    Updates

---
+++ precache-manifest.11345482a52ff6bcc72b221d4ae516b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05055d19a1a02e58d175b32fa3f178e",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "a6b64a3590f650dc2cbc",
+    "url": "/react-memory-game/static/css/main.c1a1469c.chunk.css"
+  },
+  {
+    "revision": "689649a47605e069e48a",
+    "url": "/react-memory-game/static/js/2.a3fab9b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memory-game/static/js/2.a3fab9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b64a3590f650dc2cbc",
+    "url": "/react-memory-game/static/js/main.10075dd5.chunk.js"
+  },
+  {
+    "revision": "550f7ee5b63f883502a9",
+    "url": "/react-memory-game/static/js/runtime-main.3f853a58.js"
+  }
+]);
